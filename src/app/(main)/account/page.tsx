4d2b8d90d9a4525710c31aa49d53fa9068e70ff8
Yevhenii <ke1vin.kapustian@gmail.com>
2025-12-24 14:44:55 +0100

    added some changes

--- src/app/(main)/account/page.tsx
+++ src/app/(main)/account/page.tsx
@@ -10,14 +10,15 @@ const AccountPage = async () => {
     return(
         <section>
             <div className="flex justify-between items-center">
-                <h1 className="text-5xl font-semibold">Мій профіль</h1>
-                <div className="flex gap-3">
+                <h1 className="text-5xl font-semibold max-sm:text-4xl">Мій профіль</h1>
+                <div className="flex gap-3 max-sm:flex-col">
                     <LogOutButton />
                     {isAdmin && <Link href="/account/admin" className="py-2 px-4 font-semibold black-btn animation-btn">Адмін</Link>}       
                 </div>
             </div>
             <span className='my-10 block w-full h-1 border-t border-[#e0e0e0]' />
 
+            <p className="text-center">Скоро буде...</p>
         </section>
     )
 }
